9b0991c74b31a7818d6fd1dd9a6a084b1df61802
Andre <andso041@student.liu.se>
2020-11-14 12:50:10 +0100

    Bug fix

--- Main.m
+++ Main.m
@@ -1,3 +1,6 @@
+clear 
+clc
+
 % Input
 run Data.m
 budget = 100;
@@ -8,9 +11,9 @@ price = teams(:,3)' + drivers(combs(:,1), 3)+drivers(combs(:,2), 3)+drivers(comb
 points =teams(:,2)' +drivers(combs(:,1), 2)+drivers(combs(:,2), 2)+drivers(combs(:,3), 2)+drivers(combs(:,4), 2)+drivers(combs(:,5), 2);
 
 ok_points = points.*(price <= budget);
+max_points = max(max(ok_points))
 [i,j] = ind2sub(size(ok_points),find(ok_points==max_points));
 
 % Results
-max_points = max(max(ok_points))
 team = num2team(j)
 driver = num2drv(combs(i,:))
