1fbab3eeab0722962162ee8624a9f332f97e8127
Andre <andso041@student.liu.se>
2020-11-30 12:23:17 +0100

    Added sorting and better output

--- Main.m
+++ Main.m
@@ -11,9 +11,23 @@ price = teams(:,3)' + drivers(combs(:,1), 3)+drivers(combs(:,2), 3)+drivers(comb
 points =teams(:,2)' +drivers(combs(:,1), 2)+drivers(combs(:,2), 2)+drivers(combs(:,3), 2)+drivers(combs(:,4), 2)+drivers(combs(:,5), 2);
 
 ok_points = points.*(price <= budget);
-max_points = max(max(ok_points))
+max_points = max(max(ok_points));
 [i,j] = ind2sub(size(ok_points),find(ok_points==max_points));
 
+% Sort
+[~, order] = sort(drivers(combs(i,:)', 2), "descend");
+lineup = combs(i,:);
+lineup = lineup(order);
+
 % Results
-team = num2team(j)
-driver = num2drv(combs(i,:))
+team = num2team(j);
+driver = num2drv(lineup);
+
+% Print
+disp("  Total points: " + max_points)
+disp("          Team: " + team)
+disp("   Mega driver: " + driver(1))
+disp("  Turbo driver: " + driver(2))
+disp("        Driver: " + driver(3))
+disp("        Driver: " + driver(4))
+disp("        Driver: " + driver(5))
