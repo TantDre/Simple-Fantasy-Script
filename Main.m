a7b9a9889de16bea2f17c445aa684c737ded1872
André Sollander <andre@sollander.se>
2020-12-16 23:36:01 +0100

    Better structure

--- Main.m
+++ Main.m
@@ -1,16 +1,21 @@
+addpath(genpath('Data'))
 clear 
 clc
 
-% Input
+% ------ Input ------
 run Data_2020.m
 budget = 100;
+% -------------------
 
-% Magic
+% Test all combinations
 combs = nchoosek(drivers(:,1),5);
 price = teams(:,3)' + drivers(combs(:,1), 3)+drivers(combs(:,2), 3)+drivers(combs(:,3), 3)+drivers(combs(:,4), 3)+drivers(combs(:,5), 3);
 points =teams(:,2)' +drivers(combs(:,1), 2)+drivers(combs(:,2), 2)+drivers(combs(:,3), 2)+drivers(combs(:,4), 2)+drivers(combs(:,5), 2);
 
+% Remove invalid results
 ok_points = points.*(price <= budget);
+
+% Find optimum
 max_points = max(max(ok_points));
 [i,j] = ind2sub(size(ok_points),find(ok_points==max_points));
 
